ef113df1fe925c19c1e7ba7323cba4dd72aa11e6
louie <mdesfont@student.42.fr>
2022-10-19 18:20:39 +0200

    feat(ASM): C is finished, begging of ASM part, got a functionnal Makefile

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,1 +1,17 @@
-;Colleen,s
+section .data
+	hello_world db "Hello world!", 0x0A
+
+section .text
+	global main
+
+main:
+	mov rax, 4
+	mov rdi, 1
+	mov rsi, hello_world
+	mov rdx, 13
+	syscall
+
+	; Terminate the programm
+	mov rax, 1
+	mov rdi, 0
+	syscall
