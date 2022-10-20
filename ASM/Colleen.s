948538b25ef334072ab4836c00df912749704be2
louie <mdesfont@student.42.fr>
2022-10-20 18:07:06 +0200

    feat: Colleen done in ASM

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,5 +1,6 @@
+; comment outside of the program
 section .data
-    msg     db  'Hello, world!', 0
+	code db "; comment outside of the program%1$csection .data%1$c%2$ccode db %3$c%4$s%3$c%1$c%1$csection .text%1$c%2$cglobal main%1$c%1$cextern printf%1$c%1$cmain:%1$c%2$cpush rbp%1$c%2$cmov rbp, rsp%1$c%1$c%2$ccall func%1$c; comment in the main function%1$c%2$cleave%1$c%2$cret%1$c%1$cfunc:%1$c%2$cpush rbp%1$c%2$cmov rbp, rsp%1$c%1$c%2$cmov rdi, code%1$c%2$cmov rsi, 0x0A%1$c%2$cmov rdx, 0x09%1$c%2$cmov rcx, 0x22%1$c%2$cmov r8, code%1$c%1$c%2$ccall printf%1$c%1$c%2$cleave%1$c%2$cret"
 
 section .text
 	global main
@@ -11,7 +12,7 @@ main:
 	mov rbp, rsp
 
 	call func
-
+; comment in the main function
 	leave
 	ret
 
@@ -19,8 +20,11 @@ func:
 	push rbp
 	mov rbp, rsp
 
-    mov rdi, msg
-	mov rsi, msg
+	mov rdi, code
+	mov rsi, 0x0A
+	mov rdx, 0x09
+	mov rcx, 0x22
+	mov r8, code
 
 	call printf
 
